1de243100121f5b4dafe4b71d0c1c9347405a835
spade630 <spade6301@gmail.com>
2015-12-28 18:59:08 +0900

    正規表現を修正

--- scripts/issue.js
+++ scripts/issue.js
@@ -1,29 +1,11 @@
-/*
-var Slack = require('./lib/node-slack-client');
-
-var token = 'xoxb-17446158322-rLcOkPlQSVUcDNQDQ195Ozzy';
-var autoReconnect = true;
-var autoMark = true;
-
-var slack = new Slack(token, autoReconnect, autoMark);
-
-// create_issue
-// title: hoge
-// body: hogehogehoge
-slack.on('message', function(message){
-  var channel = slack.getChannelGroupOrDMByID(message.channel);
-  var user = slack.getUserByID(message.user);
-  var text = message.txt;
-  if(/^create_issue/.test(text)){
-    text = text.split('create_issue')[1];
-    postIssue(channel, user, text);
-  }
-});
-*/
+// Description:
+//   Messing around with the today API.
+// Commands:
+//   hubot today  - Return today at random.
 
 function parseText(text){
-  var content = text.split('title: ')[1];
-  content = content.split(' body: ');
+  var content = text.split(/[\s]*title:\s?/)[1];
+  content = content.split(/[\s]*body:\s?/);
   return content;
 }
 
@@ -35,11 +17,13 @@ function postIssue(text){
 }
 
 module.exports = function(robot) {
-  return robot.hear(/hoge$/, function(msg){
+  /*return robot.hear(/hoge$/, function(msg){
     return msg.send('foo');
-  });
-  return robot.hear(/^create_issue (.+)/, function(msg){
-    var text = msg.match[1];
+  });*/
+  return robot.hear(/create_issue([\s\S]*)/, function(msg){
+    //return msg.send(msg.match[1]);
+    var text = msg.match[1].replace(/\n/g, '');
+    //return msg.send(text);
     //var user = msg.message.user.name;
     //var channel = msg.message.room;
     var content = postIssue(text);
